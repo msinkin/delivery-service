a476678b07c1837c6e534f5be3b4e660be28c223
Max <msinkin03@gmail.com>
2023-04-09 11:19:06 +0700

    tailwind headless integration & mdx pages

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,9 @@ export default defineConfig({
     strictPort: true,
     port: 8080,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    { enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */) },
+    react(),
+    tsconfigPaths()
+  ],
 })

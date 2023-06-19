e73b90a53a86404612c89d4331247ceeab4c3a57
Max <msinkin03@gmail.com>
2023-06-19 10:16:52 +0700

    update hardhat configuration

--- tracking/hardhat.config.ts
+++ tracking/hardhat.config.ts
@@ -5,7 +5,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
-      chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+      chainId: 31337
     }
   }
 };

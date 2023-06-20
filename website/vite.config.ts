392519ff79e96b9cb177a332d83b23c4be2b4fec
Max <msinkin03@gmail.com>
2023-06-20 09:41:25 +0700

    fixed fonts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import mdx from '@mdx-js/rollup'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
   server: {
     host: true,
     strictPort: true,
